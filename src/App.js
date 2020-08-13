a4a4bbb64370d47eff249fc8490de9a04b5b605b
Arifa <arifabegam.mujawar@gmail.com>
2020-08-13 13:16:43 +0300

    css applied

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import quotes from './QuotesDatabase';
 import './App.css';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [quote, setQuote] = useState(quotes[0].quote)
+const [author, setAuthor] = useState(quotes[0].author)
+
+  const randomNumber = () => {
+    const random =Math.floor(Math.random()*quotes.length)
+    console.log('random num ', random)
+
+    setQuote(quotes[random].quote)
+    setAuthor(quotes[random].author)
+  }
+  
+return (
+ 
+  <div className='container'>
+    <div className="wrapper">
+    <p className="quote">{quote}</p>
+    <p className="author">- {author? author : 'Unknown'}</p>
+    <button onClick={randomNumber}>Generate Quote</button>
     </div>
-  );
+  </div>
+)
 }
+
 
 export default App;
